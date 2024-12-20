49a8531f12b05b5d958b90c35674365bba1684b4
zachmshort <zach.short@fantomworks.com>
2024-12-19 23:49:39 -0500

    testing websocket

--- routes/transferRoutes.go
+++ routes/transferRoutes.go
@@ -2,14 +2,9 @@ package routes
 
 import (
 	"github.com/gin-gonic/gin"
-	"github.com/zachmshort/monopoly-backend/controllers"
 )
 
 func TransferRoutes(r *gin.Engine) {
-	transaction := r.Group("/transactions")
 	{
-		transaction.POST("/bank/:playerId", controllers.BankTransfer)
-		transaction.POST("/request", controllers.RequestTransfer)
-		transaction.POST("/transfer", controllers.Transfer)
 	}
 }
